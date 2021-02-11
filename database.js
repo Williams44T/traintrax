a8249d7f4792fefc92963024b9bdb0eb45814665
Travis Williams <williams44t@gmail.com>
2021-02-10 23:48:41 -0800

    adds sqlite database and access to it

---
+++ database.js
@@ -0,0 +1,51 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const { STRING, INTEGER } = DataTypes;
+
+const sequelizeOptions = {
+  logging: false,
+  dialect: 'sqlite',
+  storage: './workouts.sqlite',
+};
+const sequelize = new Sequelize(sequelizeOptions);
+
+const workoutSchema = {
+  date: STRING,
+  title: STRING,
+  bodyweight: INTEGER,
+};
+
+const exerciseSchema = {
+  name: STRING,
+  index: INTEGER,
+  date: STRING,
+};
+
+const setSchema = {
+  exercise_idx: INTEGER,
+  index: INTEGER,
+  date: STRING,
+  goal: INTEGER,
+  actual: INTEGER,
+  weight: INTEGER,
+};
+
+const schemaOptions = {
+  timestamps: false,
+};
+
+const Workout = sequelize.define('Workout', workoutSchema, schemaOptions);
+const Exercise = sequelize.define('Exercise', exerciseSchema, schemaOptions);
+const Set = sequelize.define('Set', setSchema, schemaOptions);
+
+const syncOptions = {
+  alter: true,
+};
+
+sequelize.authenticate()
+.then(() => console.log('connected to ' + sequelizeOptions.storage))
+.then(() => sequelize.sync(syncOptions))
+.catch((err) => console.log('ERROR:', err.message));
+
+module.exports = { Workout, Exercise, Set };
+
+
