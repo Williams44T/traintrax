01f167ca3703f8760d7ff20aea5cd7deb02a6848
Travis Williams <williams44t@gmail.com>
2021-02-12 00:22:55 -0800

    sets up navigation skeleton

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import {
+  ProfileScreen,
+  CalendarScreen,
+  WorkoutScreen,
+  StatsScreen,
+} from './screens';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function CalendarStackScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Calendar" component={CalendarScreen} />
+      <Stack.Screen name="Workout" component={WorkoutScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="Calendar" component={CalendarStackScreen} />
+        <Tab.Screen name="Stats" component={StatsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/index.js
@@ -0,0 +1,4 @@
+export { default as ProfileScreen } from './profile.js';
+export { default as CalendarScreen } from './calendar.js';
+export { default as WorkoutScreen } from './workout.js';
+export { default as StatsScreen } from './stats.js';
