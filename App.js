a6f380ca0f2b44af3645ed1804f64dd885be008c
Travis Williams <williams44t@gmail.com>
2021-02-12 20:24:45 -0800

    gives icons to bottom navigator and headers to all screens

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
 import {
   ProfileScreen,
   CalendarScreen,
@@ -12,6 +13,14 @@ import {
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
+function ProfileStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
+  );
+}
+
 function CalendarStackScreen() {
   return (
     <Stack.Navigator>
@@ -21,13 +30,42 @@ function CalendarStackScreen() {
   );
 }
 
+function StatsStackScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Stats" component={StatsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let name =
+      route.name === 'Profile'
+        ? 'person-circle-outline'
+        : route.name === 'Calendar'
+        ? 'calendar'
+        : 'bar-chart';
+    return <Ionicons name={name} color={color} size={size} />;
+  },
+  tabBarLabel: () => null,
+});
+
+const tabBarOptions = {
+  activeTintColor: 'tomato',
+  inactiveTintColor: 'gray',
+};
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Navigator
+        screenOptions={screenOptions}
+        tabBarOptions={tabBarOptions}
+      >
+        <Tab.Screen name="Profile" component={ProfileStackScreen} />
         <Tab.Screen name="Calendar" component={CalendarStackScreen} />
-        <Tab.Screen name="Stats" component={StatsScreen} />
+        <Tab.Screen name="Stats" component={StatsStackScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
